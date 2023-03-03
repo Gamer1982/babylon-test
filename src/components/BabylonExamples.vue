e2a3c24d51c918a2d9ac8aa59a484e7c16c2070e
Igrok82 <pplazy1982@gmail.com>
2023-03-03 13:29:37 +0300

    create scene

---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="babylon">
+    <h2 class="babylon__h2 h2">Babylon Examples</h2>
+    <canvas class="babylon__canvas canvas"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { BasicScene } from "@/BabylonExamples/BasicScene"
+
+defineProps<{
+  msg: string
+}>()
+
+const canvas = ref();
+
+onMounted(() => {
+  canvas.value = document.querySelector("canvas")!;
+  new BasicScene(canvas.value)
+})
+
+
+
+
+
+</script>
+
+
+
+<style scoped>
+.canvas {
+  width: 1280px;
+  height: 720px;
+}
+</style>
