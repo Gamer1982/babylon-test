fdddf3bf564a63481c062be5cd48652bf9e0e9eb
Igrok82 <pplazy1982@gmail.com>
2023-03-08 11:48:21 +0300

    babylon loader install

--- src/BabylonExamples/CustomModel.ts
+++ src/BabylonExamples/CustomModel.ts
@@ -1,65 +1,122 @@
-import {Scene, Engine, FreeCamera, Vector3, HemisphericLight,StandardMaterial, MeshBuilder,PBRMaterial,Texture, CubeTexture, SceneLoader} from "@babylonjs/core"
-
-export class CustomModel{
+import {
+    Scene,
+    Engine,
+    FreeCamera,
+    Vector3,
+    MeshBuilder,
+    CubeTexture,
+    Texture,
+    PBRMaterial,
+    SceneLoader,
+  } from "@babylonjs/core";
+  import "@babylonjs/loaders";
+  
+  export class CustomModel{
     scene: Scene;
-    engine:Engine;
-    constructor(private canvas:HTMLCanvasElement){
-             this.engine = new Engine(this.canvas,true)
-             this.scene = this.CreateScene()
-
-             this.engine.runRenderLoop(()=>{
-                this.scene.render()
-             })
-             this.CreateEnvirenment()
-             this.CreateBarrel()
-
+    engine: Engine;
+  
+    constructor(private canvas: HTMLCanvasElement) {
+      this.engine = new Engine(this.canvas, true);
+      this.scene = this.CreateScene();
+      this.CreateGround();
+      this.CreateBarrel();
+      this.CreateCampfire();
+  
+      this.engine.runRenderLoop(() => {
+        this.scene.render();
+      });
     }
-
-    CreateScene():Scene {
-        const scene = new Scene(this.engine)
-        const camera = new FreeCamera("camera", new Vector3(0,1,-7.5),this.scene);
-        camera.attachControl();
-        camera.speed = 0.25;
-
-
-        const pbrTexture = CubeTexture.CreateFromPrefilteredData("../../public/environment/environment4k(2).env",scene);
-        scene.environmentTexture = pbrTexture;
-        scene.createDefaultSkybox(pbrTexture,true)  
-
-        scene.environmentIntensity = 0.5;
-
-        return scene
+  
+    CreateScene(): Scene {
+      const scene = new Scene(this.engine);
+      const camera = new FreeCamera(
+        "camera",
+        new Vector3(0, 0.75, -8),
+        this.scene
+      );
+      camera.attachControl();
+      camera.speed = 0.25;
+  
+      const envTex = CubeTexture.CreateFromPrefilteredData(
+        "./environment/sky.env",
+        scene
+      );
+  
+      scene.environmentTexture = envTex;
+  
+      scene.createDefaultSkybox(envTex, true);
+  
+      scene.environmentIntensity = 0.5;
+  
+      return scene;
     }
-    CreateEnvirenment():void{
-        const ground = MeshBuilder.CreateGround("ground",{width:10,height:10},this.scene)
-
-
-
-        ground.material  = this.CreateAsphalt();
+  
+    CreateGround(): void {
+      const ground = MeshBuilder.CreateGround(
+        "ground",
+        { width: 10, height: 10 },
+        this.scene
+      );
+  
+      ground.material = this.CreateAsphalt();
     }
-
-    CreateAsphalt():PBRMaterial {
-        const pbr = new PBRMaterial("pbr",this.scene)
-
-        pbr.albedoTexture = new Texture("../../public/textures/asfalt/asphalt_02_diff_1k.jpg",this.scene);
-        pbr.bumpTexture = new Texture("../../public/textures/asfalt/asphalt_02_nor_gl_1k.jpg",this.scene);
-        pbr.metallicTexture = new Texture("../../public/textures/asfalt/asphalt_02_arm_1k.jpg")
-        pbr.invertNormalMapX=true;
-        pbr.invertNormalMapY=true;
-
-
-        pbr.roughness = 1;
-    
-
-
-
-        return pbr
+  
+    CreateAsphalt(): PBRMaterial {
+      const pbr = new PBRMaterial("pbr", this.scene);
+      pbr.albedoTexture = new Texture(
+        "../../public/textures/asfalt/asphalt_02_diff_1k.jpg",
+        this.scene
+      );
+  
+      pbr.bumpTexture = new Texture(
+        "../../public/textures/asfalt/asphalt_02_nor_gl_1k.jpg",
+        this.scene
+      );
+  
+      pbr.invertNormalMapX = true;
+      pbr.invertNormalMapY = true;
+  
+      pbr.useAmbientOcclusionFromMetallicTextureRed = true;
+      pbr.useRoughnessFromMetallicTextureGreen = true;
+      pbr.useMetallnessFromMetallicTextureBlue = true;
+  
+      pbr.metallicTexture = new Texture(
+        "../../public/textures/asfalt/asphalt_02_arm_1k.jpg",
+        this.scene
+      );
+  
+      return pbr;
     }
-    CreateBarrel():void {
-        SceneLoader.ImportMesh("","../../public/models/","barrel.glb",this.scene,(meshes)=>{
-            console.log("meshes", meshes);
-            
-        })
-
+  
+    async CreateBarrel(): Promise<void> {
+      SceneLoader.ImportMesh(
+        "",
+        "./models/",
+        "barrel.glb",
+        this.scene,
+        (meshes) => {
+          console.log("meshes", meshes);
+        }
+      );
+  
+      const { meshes } = await SceneLoader.ImportMeshAsync(
+        "",
+        "./models/",
+        "barrel.glb"
+      );
+  
+      console.log("meshes", meshes);
     }
-} 
+  
+    async CreateCampfire(): Promise<void> {
+      const models = await SceneLoader.ImportMeshAsync(
+        "",
+        "./models/",
+        "campfire.glb"
+      );
+  
+      // models.meshes[0].position = new Vector3(-15, 0, 0);
+  
+      console.log("models", models);
+    }
+  }
